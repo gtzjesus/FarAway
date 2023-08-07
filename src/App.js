1c7febd6a7d7c5ba2d0a68293cb7aea0156f2f28
gtz.jesus <gtz.jesus@outlook.com>
2023-08-07 10:23:18 -0600

    Lifting State Up and Reviewing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 4, packed: true },
-];
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -18,17 +20,18 @@ export default function App() {
 function Logo() {
   return <h1>🌲 Far Far Far Away 🧳</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    // GUARD CLOUSE
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
+
+    onAddItems(newItem);
 
     // MAKE FORM GO INTO DEFAULT STATE (SYNC)
     setDescription('');
@@ -58,11 +61,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
