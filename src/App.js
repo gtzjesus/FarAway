f6d3005b3a7ec2c991d75e2df60f865c2d0f6395
gtz.jesus <gtz.jesus@outlook.com>
2023-08-07 11:15:01 -0600

    Sorting Items w Derived State

--- src/App.js
+++ src/App.js
@@ -78,10 +78,23 @@ function Form({ onAddItems }) {
   );
 }
 function PackingList({ items, onDeleteItem, onHandleToggleItem }) {
+  const [sortBy, setSortBy] = useState('packed');
+  let sortedItems;
+
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === 'packed')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -90,9 +103,21 @@ function PackingList({ items, onDeleteItem, onHandleToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(event) => setSortBy(event.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
+
 function Item({ item, onDeleteItem, onHandleToggleItem }) {
   return (
     <li>
